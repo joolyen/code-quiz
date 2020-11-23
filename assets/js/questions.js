7a2358842a6931b4d84fb4be0adc89249b1eb661
joolyenIV <julianivey@gmail.com>
2020-11-22 20:39:43 -0600

    built out questions array that will be needed and created stylesheet

---
+++ assets/js/questions.js
@@ -0,0 +1,28 @@
+// An array of all of the question information needed
+var questions = [
+    {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["Strings", "Booleans", "Alerts", "Numbers"],
+      answer: "Alerts"
+    },
+    {
+      title: "The condition in an if / else statement is enclosed within ______.",
+      choices: ["Quotes", "Curly Brackets", "Parentheses", "Square Brackets"],
+      answer: "Parentheses"
+    },
+    {
+      title: "Arrays in JavaScript can be used to store ______.",
+      choices: ["Numbers and Strings", "Other Arrays", "Booleans", "All of the Above"],
+      answer: "All of the Above"
+    },
+    {
+      title: "String values must be enclosed within ______ when being assigned to variables.",
+      choices: ["Commas", "Curly Brackets", "Quotes", "Parentheses"],
+      answer: "Quotes"
+    },
+    {
+      title: "A very useful tool used during development and debugging for printing content to the debugger is:",
+      choices: ["JavaScript", "Terminal / Bash", "For Loops", "Console.log"],
+      answer: "Console.log"
+    }
+  ];
